696fcbd4a55887fd8f355f9f7cc8e3f8c2402509
William Owalla <william.owalla@student.moringaschool.com>
2022-05-17 16:34:22 +0300

    deployment attempt commit

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
+    
     return dict(app = app,db = db,User = User, Blogs = Blogs, Follower = Follower, Comments = Comments)
 
 if __name__ == '__main__':
