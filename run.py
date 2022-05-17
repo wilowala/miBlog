73f3ace767d9f8f3027cc957367298ee97808052
William Owalla <william.owalla@student.moringaschool.com>
2022-05-17 16:00:18 +0300

    refactor app's main runpoint

---
+++ run.py
@@ -0,0 +1,19 @@
+from app import create_app, db
+from flask_script import Manager,Server
+from app.models import User,Blogs,Comments, Follower
+from flask_migrate import Migrate, MigrateCommand
+
+
+app = create_app('prod')
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Blogs = Blogs, Follower = Follower, Comments = Comments)
+
+if __name__ == '__main__':
+    manager.run()
