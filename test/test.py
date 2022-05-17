a12e7293df467485fd7a3f6f28abe2b9770835c4
William Owalla <william.owalla@student.moringaschool.com>
2022-05-17 16:10:23 +0300

    run app tests

---
+++ test/test.py
@@ -0,0 +1,35 @@
+import unittest
+from app.models import User,Blogs, Comments
+
+class UserModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User("Michelle", "michelle@gmail.com", "1234")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_user, User))
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.password is not None)
+    
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.secure_password
+
+class BlogModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_blog = Blogs("Blog Title","Blog Body","User ID")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_blog, Blogs))
+
+
+class CommentModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_comment = Comments("User ID","Comment Body","Pitch ID")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_comment, Comments))
+
+   
+if __name__ == '__main__':
+    unittest.main()
