d5d069f1957b2bb81e4804d8667d85f57badec75
William Owalla <william.owalla@student.moringaschool.com>
2022-05-17 19:40:20 +0300

    final build

--- app/main/views.py
+++ app/main/views.py
@@ -119,7 +119,6 @@ def subscribe():
     email = request.form.get('subscriber')
     new_subscriber = Follower(email = email)
     new_subscriber.save_subscriber()
-    # mail_message("Subscribed to D-Blog","email/welcome_subscriber",new_subscriber.email,new_subscriber=new_subscriber)
     flash('Sucessfuly subscribed')
     return redirect(url_for('main.index'))
 
